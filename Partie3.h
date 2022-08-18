ce7b59bae138133405e04d0ee3807ec03b2c9f4c
KevinMunto <104222984+KevinMunto@users.noreply.github.com>
2022-08-18 19:24:56 +0200

    Add files via upload

---
+++ Partie3.h
@@ -0,0 +1,21 @@
+//
+// Created by kevmu on 10/04/2022.
+//
+
+#ifndef PROJETTAKUZU_PARTIE3_H
+#define PROJETTAKUZU_PARTIE3_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int nb_aleatoire_0_2(); // generer un nombre entre 0 et 2
+int nb_aleatoire_3_5(); // generer un nombre entre 3 et 5
+int conversion_bin4x4();
+int conversion_bin8x8();
+void display_tab4x4();
+void afficher_matrice();
+int nb_aleatoire_0_23();
+void gen4x4();
+void gen8x8();
+
+#endif //PROJETTAKUZU_PARTIE3_H
