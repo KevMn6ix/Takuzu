7c557c9d3af4e0de200658ceb7f513fad6cdbdb5
KevinMunto <104222984+KevinMunto@users.noreply.github.com>
2022-08-18 19:22:14 +0200

    Add files via upload

---
+++ Partie1.h
@@ -0,0 +1,18 @@
+//
+// Created by kevmu on 23/04/2022.
+//
+
+#ifndef PROJETTAKUZU_PARTIE1_H
+#define PROJETTAKUZU_PARTIE1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void matsolution();
+void auto_masque();
+void init_grille_jeu();
+void jeupartie1();
+
+void ajout_affichage_et_verif();
+
+#endif //PROJETTAKUZU_PARTIE1_H
