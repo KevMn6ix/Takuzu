118aac1d1fbea0f6bbb5cf93bebe2978d44856f2
KevinMunto <104222984+KevinMunto@users.noreply.github.com>
2022-08-18 19:26:38 +0200

    Add files via upload

---
+++ TakuzuRules.h
@@ -0,0 +1,30 @@
+//
+// Created by kevmu on 28/04/2022.
+//
+
+#ifndef PROJETTAKUZU_TAKUZURULES_H
+#define PROJETTAKUZU_TAKUZURULES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int droite();
+int gauche();
+int dessous();
+int dessus();
+int gauche_et_droite();
+
+int verifverticale();
+int verifhorizontale();
+int entredeuxverticalhorizontale();
+
+int compteur0ou1h();
+int compteur0ou1v();
+
+int parite0et1();
+int compLigne();
+
+int egalite_matrice();
+
+
+#endif //PROJETTAKUZU_TAKUZURULES_H
