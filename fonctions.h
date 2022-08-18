73b70cbd914bea2baa3da3acfea4be149c31d858
KevinMunto <104222984+KevinMunto@users.noreply.github.com>
2022-08-18 19:25:49 +0200

    Add files via upload

---
+++ fonctions.h
@@ -0,0 +1,16 @@
+//
+// Created by kevmu on 08/04/2022.
+//
+
+#ifndef PROJETTAKUZU_FONCTIONS_H
+#define PROJETTAKUZU_FONCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void Color();
+
+int** creer_mat();
+void remplir_matrice_basique();
+void liberer_mat();
+
+#endif //PROJETTAKUZU_FONCTIONS_H
